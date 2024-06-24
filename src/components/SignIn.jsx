67f40b54cb049a377c88bec04f123182c76ecc42
Rui Gomes <yruigomes99@gmail.com>
2024-06-24 18:14:47 +0100

    Update routes and ports

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -9,7 +9,7 @@ function SignIn() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:3000/signin', {
+    const response = await fetch('http://localhost:3001/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
