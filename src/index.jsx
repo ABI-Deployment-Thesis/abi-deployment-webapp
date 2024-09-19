a6417da1cea5ffc86495e8a9027494c12e58af12
ruigomes99 <yruigomes99@gmail.com>
2024-09-19 20:45:33 +0100

    test 2

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// This is a test
+// This is a test - v2
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
